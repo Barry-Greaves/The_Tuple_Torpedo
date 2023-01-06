5321a2ca77e2fd10c4c7f14f64e9c683c90bb8f8
Barry-Greaves <musiknonstop909@gmail.com>
2023-01-06 01:06:06 +0000

    Contruct game class that defines difficulty level, board size and number of ships

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
 import random
+
+class TupleTorpedo:
+    """
+    Contruct game class that defines difficulty level,
+    board size and number of ships
+    """
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.board_size = 0
+        self.num_ships = 0
+        self.player_board = []
+        self.computer_board = []
+        self.hidden_computer_board = []
+        self.player_ships = []
+        self.computer_ships = []
